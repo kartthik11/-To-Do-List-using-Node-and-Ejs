634e89102d733f5739d5e84bf4705132563e8bbc
kartthik11 <83079599+kartthik11@users.noreply.github.com>
2021-07-06 18:40:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+//jshint esversion:6
+
+const express = require ("express");
+
+const bodyParser = require("body-parser");
+const { request } = require("express");
+
+const app = express();
+
+let items = ["Work", "Study and Code", "Eat"];
+let workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req , res){
+
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+
+    let day = today.toLocaleDateString("en-US", options)
+
+    res.render("list", {listTitle: day, newListItems: items});
+    
+});
+
+
+
+app.post("/", function(req, res){
+    
+    let item = req.body.newItem;
+    
+    if (req.body.list == "work"){
+        workItems.push(item);
+        res.redirect("/work");
+    }else{
+        items.push(item);
+        res.redirect("/");
+    }
+});
+
+app.get("/work", function(req, res){
+    res.render("list",{listTitle: "Work List", newListItems: workItems});
+});
+
+
+app.listen(3000, function(){
+    console.log("**server is started at 3000!**");
+});
